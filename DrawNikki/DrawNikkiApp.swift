185d8aae3bfab7aa85574e2a6eeceb55e4d368d4
hn-carter-mac <84824787+hn-carter@users.noreply.github.com>
2021-11-14 23:22:33 +0900

    create xcode project

---
+++ DrawNikki/DrawNikkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawNikkiApp.swift
+//  DrawNikki
+//
+//  Created by hn-carter on 2021/11/14.
+//
+
+import SwiftUI
+
+@main
+struct DrawNikkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
