df57578688e0795e493852871fbffcfda57cbfdc
hn-carter-mac <84824787+hn-carter@users.noreply.github.com>
2021-12-02 23:27:37 +0900

    CoreDataからの取得処理を試作

--- DrawNikki/Models/FileNumber.swift
+++ DrawNikki/Models/FileNumber.swift
@@ -6,7 +6,45 @@
 //
 
 import Foundation
+import CoreData
+import os
 
-public class FileNumber {
+struct FileNumberRecord {
+    var number: Int?
+    var created_at: Date?
+    var updated_at: Date?
+
+    init(number: Int?) {
+        self.number = number
+        self.created_at = nil
+        self.updated_at = nil
+    }
+}
+
+
+struct FileNumberRepository {
+    // CoreDataをカプセル化したコンテナ
+    let container: NSPersistentContainer
     
+    let logger = Logger(subsystem: "DrawNikki.FileNumberRepository", category: "FileNumberRepository")
+    
+    init(controller: PersistenceController) {
+        container = controller.container
+    }
+    
+    /// CoreDataからファイルにつけた最終番号を取s得する
+    /// - Returns: 最後にファイルにつけた番号
+    func getFileNumber() -> Int {
+        var items:[File_number] = []
+        let request: NSFetchRequest = File_number.fetchRequest()
+        do {
+            items = try container.viewContext.fetch(request)
+        } catch {
+            logger.error("error in fetching data from File_number")
+        }
+        if items.count == 0 {
+            return 0
+        }
+        return Int(items[0].number)
+    }
 }
