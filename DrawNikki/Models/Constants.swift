4440e87e15e312c2ce35ede3bc7eb8c4270547f8
hn-carter-mac <84824787+hn-carter@users.noreply.github.com>
2021-12-22 23:36:55 +0900

    add 定数定義ファイル

---
+++ DrawNikki/Models/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DrawNikki
+//
+//  Created by hn-carter on 2021/12/22.
+//
+
+import Foundation
+
+struct Constants {
+    // 保存に使用するカレンダー
+    static let dbCalendar = Calendar(identifier: .gregorian)
+}
