26c3c9912407453745dad268248696db69858bd1
hn-carter-mac <84824787+hn-carter@users.noreply.github.com>
2022-01-11 16:37:32 +0900

    不足ファイル

---
+++ DrawNikki/Models/NikkiContent.swift
@@ -0,0 +1,13 @@
+//
+//  NikkiContent.swift
+//  DrawNikki
+//
+//  Created by hn-carter on 2022/01/11.
+//
+
+import Foundation
+
+struct NikkiContent: Codable {
+    var Date: String
+    var Text: String
+}
